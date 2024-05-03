5b8adf70dc9e3e60ffed4ea845577cb958a7cc02
Ginea Merrill <gineakanoe@gmail.com>
2024-05-02 21:23:06 -0400

    added more javascript functionality to add books and delete them; i.e. making the buttons work.

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,13 +3,25 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
 const li = document.createElement('li');
-
 const deleteButton = document.createElement('button');
 
-li.textContent = input.ariaValueMax;
-
+li.textContent = input.value;
 deleteButton.textContent = 'X';
-
 li.append(deleteButton);
-
 list.append(li);
+
+button.addEventListener('click', function() {
+    if (input.value.trim() !== '') {
+        list.appendChild(li);
+        input.focus();
+    }
+});
+
+deleteButton.addEventListener('click', function() {
+    list.removeChild(li);
+    input.focus();
+});
+
+input.value = '';
+
+input.focus();
