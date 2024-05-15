e369af4d58be96e2b0990ce11feddb2abcfaa934
Ginea Merrill <gineakanoe@gmail.com>
2024-05-15 18:44:09 -0400

    week 4 javascript

---
+++ week04/scripts/jsObjects.js
@@ -0,0 +1,12 @@
+// courses.js
+const aCourse = {
+    code: "CSE121b",
+    name: "Javascript Language",
+    sections: [
+        {sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTH', instructor: 'Bro T'},
+        {sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTH', instructor: 'Sis A'},
+    ],
+};
+
+
+
